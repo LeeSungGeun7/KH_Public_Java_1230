05ccc668c0ba1146ea121ad43d6fe5ac80d63c62
LSG <sungekno3o@gmail.com>
2023-01-04 21:31:51 +0900

    회원정보예제연습

---
+++ src/회원정보예제/MemberInfo.java
@@ -0,0 +1,14 @@
+package 회원정보예제;
+// 이름 , 나이, 직업, 성별의 정보를 입력받기
+public class MemberInfo {
+    public static void main(String[] args) {
+        Member member = new Member();
+        member.setName();
+        member.setGender();
+        member.setAge();
+
+
+
+    }
+
+}
